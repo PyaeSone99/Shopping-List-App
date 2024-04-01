960774cf371773ce5eee62da73a809f1319ced1e
pyaesoneaung <pyaesone@blue-stone.net>
2024-04-02 04:38:52 +0630

    212 - Extracting Entered Values

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/category.dart';
+import 'package:shopping_list/models/category.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem ({super.key});
@@ -13,10 +14,17 @@ class NewItem extends StatefulWidget {
 class _NewItemState extends State<NewItem>{
 
   final _formKey = GlobalKey<FormState>();
+  var _enteredName = '';
+  var _enteredQuantity = 1;
+  var _selectedCategory = categories[Categories.vegetables]!;
 
   void _saveItem(){
-    _formKey.currentState!.validate();
-    _formKey.currentState!.save();
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      print(_enteredName);
+      print(_enteredQuantity);
+      print(_selectedCategory.title);
+    }
   }
 
   @override
@@ -42,7 +50,9 @@ class _NewItemState extends State<NewItem>{
                   }
                   return null;
                 },
-                onSaved: (value){},
+                onSaved: (value){
+                  _enteredName = value!;
+                },
               ), //Instead of textField
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -60,11 +70,15 @@ class _NewItemState extends State<NewItem>{
                         }
                         return null;
                       },
+                      onSaved: (value){
+                        _enteredQuantity = int.parse(value!);
+                      },
                     ),
                   ),
                   const SizedBox(width: 8,),
                   Expanded(
                     child: DropdownButtonFormField(
+                      value: _selectedCategory,
                       items: [
                         for(final category in categories.entries)
                           DropdownMenuItem(
@@ -83,7 +97,10 @@ class _NewItemState extends State<NewItem>{
                           )
                       ],
                       onChanged: (value){
-                    
+                        // setState(() {
+                        //   _selectedCategory = value!;
+                        // });
+                        _selectedCategory = value!;
                       },
                     ),
                   ),

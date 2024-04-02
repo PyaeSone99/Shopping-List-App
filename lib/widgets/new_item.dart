eedad00b94549cd174ebabe864c4a2af0ebeedee
Pyae Sone Aung <pyaesone@blue-stone.net>
2024-04-02 13:36:23 +0630

    222 - Working with the Request Waiting for the Response

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/category.dart';
 import 'package:shopping_list/models/category.dart';
-import 'package:shopping_list/models/grocery_item.dart';
+// import 'package:shopping_list/models/grocery_item.dart';
 import 'package:http/http.dart' as http;
 
 class NewItem extends StatefulWidget {
@@ -22,14 +22,14 @@ class _NewItemState extends State<NewItem>{
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
 
       final url = Uri.https(
           'flutter-shopping-list-11108-default-rtdb.firebaseio.com',
           'shopping-list.json');
-      http.post(url,
+      final response = await http.post(url,
           headers: {
             'Content-Type': 'application/json',
           },
@@ -40,8 +40,11 @@ class _NewItemState extends State<NewItem>{
               'category': _selectedCategory.title
             },
           ));
-
-      // Navigator.of(context).pop();
+      print(response.statusCode);
+      if (!mounted) {
+        return;
+      }
+      Navigator.of(context).pop();
     }
   }
 
